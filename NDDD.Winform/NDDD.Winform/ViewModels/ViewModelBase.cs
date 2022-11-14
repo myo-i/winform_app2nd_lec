1366b9a94b66c0f7c8694abc641c39a70802e9f3
myo-i <Imiyo0521@gmail.com>
2022-11-15 01:18:47 +0900

    ✨ ViewModelBaseの作成

---
+++ NDDD.Winform/NDDD.Winform/ViewModels/ViewModelBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+
+namespace NDDD.WinForm.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field,
+            T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            var h = this.PropertyChanged;
+            if (h != null)
+            {
+                h(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+            return true;
+        }
+
+
+        public virtual DateTime GetDateTime()
+        {
+            return DateTime.Now;
+        }
+
+
+        public void OnPropertyChanged()
+        {
+            PropertyChanged.Invoke(
+                this,
+                new PropertyChangedEventArgs(""));
+        }
+    }
+}
+
