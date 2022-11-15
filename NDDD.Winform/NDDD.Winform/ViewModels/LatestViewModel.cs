c5116ea3a4f3d25f35ccdbca9fec3a7d1007e987
myo-i <Imiyo0521@gmail.com>
2022-11-15 23:01:32 +0900

    ✨ LatestViewModel追加

---
+++ NDDD.Winform/NDDD.Winform/ViewModels/LatestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using NDDD.WinForm.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.Winform.ViewModels
+{
+    public class LatestViewModel : ViewModelBase
+    {
+    }
+}
