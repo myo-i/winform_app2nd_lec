f611787bfe24a2ba431f4758eb66d857a2e89971
myo-i <Imiyo0521@gmail.com>
2022-11-15 23:01:05 +0900

    ✨ LatestViewModelのテスト追加

---
+++ NDDD.Winform/NDDDTest.Tests/ViewModelTests/LatestViewModelTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NDDD.Winform.ViewModels;
+using System;
+
+namespace NDDDTest.Tests.ViewModelTests
+{
+    [TestClass]
+    public class LatestViewModelTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var vm = new LatestViewModel();
+        }
+    }
+}
