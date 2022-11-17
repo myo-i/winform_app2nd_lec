06326d603a1a6dcee6c210e450d1d490936cf67f
myo-i <Imiyo0521@gmail.com>
2022-11-18 00:22:11 +0900

    ✨ DB接続の代替、Fakeの仮実装

---
+++ NDDD.Winform/NDDD.Infrastructure/Fake/MeasureFake.cs
@@ -0,0 +1,13 @@
+﻿using NDDD.Domain.Entities;
+using NDDD.Domain.Repositories;
+
+namespace NDDD.Infrastructure.Fake
+{
+    public class MeasureFake : IMeasureRepository
+    {
+        public MeasureEntity GetLatest()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
