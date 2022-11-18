a8b02d2a3f355d71b3e991c04c52be3c1046ab66
myo-i <Imiyo0521@gmail.com>
2022-11-19 01:24:43 +0900

    ✨ DomainのAP構成ファイルを設定し、SharedでAP構成ファイルの値を呼び出すように設定

--- NDDD.Winform/NDDD.Domain/Shared.cs
+++ NDDD.Winform/NDDD.Domain/Shared.cs
@@ -1,7 +1,11 @@
-﻿namespace NDDD.Domain
+﻿using System.Configuration;
+
+namespace NDDD.Domain
 {
     public class Shared
     {
-        public static bool IsFake { get; } = true;
+        // アプリケーション構成ファイルで設定したキー"IsFake"が"1"の時にtrue
+        public static bool IsFake { get; } =
+            ConfigurationManager.AppSettings["IsFake"] == "1";
     }
 }
