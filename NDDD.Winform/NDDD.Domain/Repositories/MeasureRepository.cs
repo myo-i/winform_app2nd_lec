c1b3f8272f413f95bc200769a29a5c62ad6a44b1
myo-i <Imiyo0521@gmail.com>
2022-11-20 23:59:37 +0900

    ✨ GetLatestで取得した値を編集できる、具象クラスMeasureRepositoryの作成

---
+++ NDDD.Winform/NDDD.Domain/Repositories/MeasureRepository.cs
@@ -0,0 +1,42 @@
+﻿using NDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NDDD.Domain.Repositories
+{
+    public sealed class MeasureRepository
+    {
+        private IMeasureRepository _repository;
+
+        public MeasureRepository(IMeasureRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public MeasureEntity GetLatest()
+        {
+            var val1 = _repository.GetLatest();
+            Thread.Sleep(1000);
+            var val2 = _repository.GetLatest();
+            Thread.Sleep(1000);
+            var val3 = _repository.GetLatest();
+            Thread.Sleep(1000);
+
+            var sum = 
+                val1.MeasureValue.Value + 
+                val2.MeasureValue.Value + 
+                val3.MeasureValue.Value;
+
+            var ave = sum / 3f;
+
+            return new MeasureEntity(
+                val3.AreaId.Value,
+                val3.MeasureDate.Value,
+                ave);
+        }
+    }
+}
