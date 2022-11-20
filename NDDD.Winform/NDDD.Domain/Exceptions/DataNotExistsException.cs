5b2dd1dddeb70771ee9bb1815c1e4a505af064b2
myo-i <Imiyo0521@gmail.com>
2022-11-21 00:27:24 +0900

    ✨ Exceptionの作成

---
+++ NDDD.Winform/NDDD.Domain/Exceptions/DataNotExistsException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NDDD.Domain.Exceptions
+{
+    public sealed class DataNotExistsException : Exception
+    {
+
+        public DataNotExistsException() 
+            :base("データがありません")
+        { 
+        }
+    }
+}
