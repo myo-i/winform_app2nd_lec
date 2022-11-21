4adbe6c0c4bd858ef16b5c38dc6f4377e95a4e6b
myo-i <Imiyo0521@gmail.com>
2022-11-22 02:59:54 +0900

    ⚡️ 例外の区分に応じて表示するメッセージのアイコンやキャプションを設定

--- NDDD.Winform/NDDD.Winform/Views/LatestView.cs
+++ NDDD.Winform/NDDD.Winform/Views/LatestView.cs
@@ -1,4 +1,5 @@
-﻿using NDDD.Winform.ViewModels;
+﻿using NDDD.Domain.Exceptions;
+using NDDD.Winform.ViewModels;
 using System;
 using System.Windows.Forms;
 
@@ -36,7 +37,28 @@ namespace NDDD.Winform.Views
             }
             catch(Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBoxIcon icon = MessageBoxIcon.Error;
+                // captionはメッセージボックスのタイトル
+                string caption = "エラー";
+
+                // exの値が変換できれば値が入り、変換できなければ値がnullになる
+                // 恐らく、ExceptionBaseを継承した例外であれば値が入り、それ以外であればnullになる
+                var exceptionBase = ex as ExceptionBase;
+                if (exceptionBase != null)
+                {
+                    if(exceptionBase.Kind == ExceptionBase.ExceptionKind.Info)
+                    {
+                        icon = MessageBoxIcon.Information;
+                        caption = "情報";
+                    }
+                    else if (exceptionBase.Kind == ExceptionBase.ExceptionKind.Warning)
+                    {
+                        icon = MessageBoxIcon.Warning;
+                        caption = "警告";
+
+                    }
+                }
+                MessageBox.Show(ex.Message, caption, MessageBoxButtons.OK, icon);
             }
         }
     }
