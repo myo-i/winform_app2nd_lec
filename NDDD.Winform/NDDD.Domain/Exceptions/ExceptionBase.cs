e9103e22a4f6603104ee603a3edc68a5fe5bd8ce
myo-i <Imiyo0521@gmail.com>
2022-11-22 02:55:46 +0900

    ✨ 作成した例外に区分を必ず返させるためのベースクラスを作成＋冗長性回避のためのコンストラクタ実装

---
+++ NDDD.Winform/NDDD.Domain/Exceptions/ExceptionBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.Domain.Exceptions
+{
+    public abstract class ExceptionBase : Exception
+    {
+        // Exception
+        public ExceptionBase(string message) : base(message)
+        {
+        }
+
+        // InnerException
+        public ExceptionBase(string message, Exception exception) 
+            : base(message, exception)
+        {
+        }
+
+        // このクラスを継承したクラスはKindを返却しないと
+        // コンパイルが通らないので必ず区分を持つことになる
+        public abstract ExceptionKind Kind {get;}
+        public enum ExceptionKind
+        {
+            Info,
+            Warning,
+            Error,
+        }
+    }
+}
