8d62284e6e36f420975e38b72e399fec5cae51d5
myo-i <Imiyo0521@gmail.com>
2022-11-24 00:18:33 +0900

    ✨ 細かいロジックを追加する場合のフォルダ構成の例

---
+++ NDDD.Winform/NDDD.Domain/Logics/MeasureAnalysis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.Domain.Logics
+{
+    public static class MeasureAnalysis
+    {
+        // 例えば直近値を分析するなどの細かいロジックを追加する場合は
+        // staticなクラスとメソッドで定義して分離したほうがわかりやすい時もある
+        public static float Execute(
+            float value1,
+            float value2,
+            float value3,
+            float value4,
+            float value5)
+        {
+            return 999f;
+        }
+    }
+}
